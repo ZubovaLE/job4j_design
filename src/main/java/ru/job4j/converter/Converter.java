092d0c171e0990f1a99cc5924fb8e5925b3d13a7
Любовь <n.lubovj@gmail.com>
2022-02-26 23:14:54 +0300

    Test2 for Converter

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -10,6 +10,30 @@ public class Converter {
         return value / 80.42F;
     }
 
+    public static void doTaskOne() {
+        float euro = Converter.rubleToEuro(450);
+        float dollar = Converter.rubleToDollar(450);
+        //System.out.println("450 rubles is " + Converter.rubleToDollar(450) + " dollars");
+        System.out.println("450 rubles is " + dollar + " dollars");
+        System.out.println(euro + " euro");
+    }
+
+    public static void doTestForRubleToEuro() {
+        float in = 181.76F;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("The first test result is: " + passed);
+    }
+
+    public static void doTestForRubbleToDollar() {
+        float in = 160;
+        float expected = 2;
+        float out = Converter.rubleToDollar(in);
+        boolean passed = expected == out;
+        System.out.println("The second test result is " + passed);
+    }
+
     public static void main(String[] args) {
         Converter.doTaskOne();
         Converter.doTestForRubleToEuro();

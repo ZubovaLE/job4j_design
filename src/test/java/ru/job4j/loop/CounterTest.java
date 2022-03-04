bd6c5dc70dcfa7f1327dd93460a7fe990bbeb0f1
Любовь <n.lubovj@gmail.com>
2022-03-04 22:46:10 +0300

    Add sumEven method and test it

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -56,4 +56,44 @@ class CounterTest {
         int expected = 9;
         assertEquals(expected, out);
     }
+
+    @Test
+    @DisplayName("Test the sumByEven method when start = 1, finish = 10, then sum = 30")
+    void sumByEvenWhenStartIs1AndFinishIs10Then30() {
+        int start = 1;
+        int finish = 10;
+        int out = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertEquals(expected, out);
+    }
+
+    @Test
+    @DisplayName("Test the sumByEven method when start = -2, finish = 2, then sum = 0")
+    void sumByEvenWhenStartIsMinus2AndFinishIs2Then0() {
+        int start = -2;
+        int finish = 2;
+        int out = Counter.sumByEven(start, finish);
+        int expected = 0;
+        assertEquals(expected, out);
+    }
+
+    @Test
+    @DisplayName("Test the sumByEven method when start = 0, finish = 0, then sum = 0")
+    void sumByEvenWhenStartIs0AndFinish0Then0() {
+        int start = 0;
+        int finish = 0;
+        int out = Counter.sumByEven(start, finish);
+        int expected = 0;
+        assertEquals(expected, out);
+    }
+
+    @Test
+    @DisplayName("Test the sumByEven method when start = -4, finish = 3, then sum = -4")
+    void sumByEvenWhenStartIsMinus4AndFinishIs3ThenMinus4() {
+        int start = -4;
+        int finish = 3;
+        int out = Counter.sumByEven(start, finish);
+        int expected = -4;
+        assertEquals(expected, out);
+    }
 }

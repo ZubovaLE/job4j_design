87d1f250b68f2d3397a8df97489e3582f73ec691
NikiforovaLE <n.lubovj@gmail.com>
2022-08-13 21:03:43 +0300

    Refactor the AnalyzeTest class and add new tests

--- job4j_design/src/test/java/ru/job4j/question/AnalyzeTest.java
+++ job4j_design/src/test/java/ru/job4j/question/AnalyzeTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.question;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Set;
@@ -8,11 +10,20 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 class AnalyzeTest {
+    private User u1;
+    private User u2;
+    private User u3;
+
+    @BeforeEach
+    void initData() {
+        u1 = new User(1, "A");
+        u2 = new User(2, "B");
+        u3 = new User(3, "C");
+    }
+
     @Test
+    @DisplayName("Test diff when set is not changed")
     void whenNotChanged() {
-        User u1 = new User(1, "A");
-        User u2 = new User(2, "B");
-        User u3 = new User(3, "C");
         Set<User> previous = Set.of(u1, u2, u3);
         Set<User> current = Set.of(u1, u2, u3);
         assertThat(
@@ -22,10 +33,8 @@ class AnalyzeTest {
     }
 
     @Test
-    void whenOneChanged() {
-        User u1 = new User(1, "A");
-        User u2 = new User(2, "B");
-        User u3 = new User(3, "C");
+    @DisplayName("Test diff when one element is changed")
+    void whenOneIsChanged() {
         Set<User> previous = Set.of(u1, u2, u3);
         Set<User> current = Set.of(u1, new User(2, "BB"), u3);
         assertThat(
@@ -35,10 +44,8 @@ class AnalyzeTest {
     }
 
     @Test
-    void whenOneDeleted() {
-        User u1 = new User(1, "A");
-        User u2 = new User(2, "B");
-        User u3 = new User(3, "C");
+    @DisplayName("Test diff when one element is deleted")
+    void whenOneIsDeleted() {
         Set<User> previous = Set.of(u1, u2, u3);
         Set<User> current = Set.of(u1, u3);
         assertThat(
@@ -48,15 +55,93 @@ class AnalyzeTest {
     }
 
     @Test
-    void whenOneAdded() {
-        User u1 = new User(1, "A");
-        User u2 = new User(2, "B");
-        User u3 = new User(3, "C");
+    @DisplayName("Test diff when one element is added")
+    void whenOneIsAdded() {
         Set<User> previous = Set.of(u1, u2, u3);
         Set<User> current = Set.of(u1, u2, u3, new User(4, "D"));
         assertThat(
                 Analyze.diff(previous, current),
                 is(new Info(1, 0, 0))
+        );
+    }
+
+    @Test
+    @DisplayName("Test diff when all elements are changed")
+    public void whenAllElementsAreChanged() {
+        Set<User> previous = Set.of(u1, u2, u3);
+        Set<User> current = Set.of(new User(1, "AA"), new User(2, "BB"), new User(3, "CC"));
+        assertThat(
+                Analyze.diff(previous, current),
+                is(new Info(0, 3, 0))
+        );
+    }
+
+    @Test
+    @DisplayName("Test diff when all elements are deleted")
+    public void whenAllElementsAreDeleted() {
+        Set<User> previous = Set.of(u1, u2, u3);
+        Set<User> current = Set.of();
+        assertThat(
+                Analyze.diff(previous, current),
+                is(new Info(0, 0, 3))
+        );
+    }
+
+    @Test
+    @DisplayName("Test diff when new elements are added")
+    public void whenNewElementsAreAdded() {
+        User u4 = new User(4, "D");
+        User u5 = new User(5, "E");
+        User u6 = new User(6, "F");
+        Set<User> previous = Set.of(u1, u2, u3);
+        Set<User> current = Set.of(u1, u2, u3, u4, u5, u6);
+        assertThat(
+                Analyze.diff(previous, current),
+                is(new Info(3, 0, 0))
+        );
+    }
+
+    @Test
+    @DisplayName("Test diff when one el is changed, other el is deleted and new one is added")
+    public void whenOneIsChangedOneIsDeletedAndNewIsAdded() {
+        Set<User> previous = Set.of(u1, u2, u3);
+        Set<User> current = Set.of(new User(1, "AA"), u2, new User(4, "D"));
+        assertThat(
+                Analyze.diff(previous, current),
+                is(new Info(1, 1, 1))
+        );
+    }
+
+    @Test
+    @DisplayName("Test diff when one el is changed and new one is added")
+    public void whenChange1ElAdd1El() {
+        Set<User> previous = Set.of(u1, u2);
+        Set<User> current = Set.of(new User(1, "AA"), u2, u3);
+        assertThat(
+                Analyze.diff(previous, current),
+                is(new Info(1, 1, 0))
+        );
+    }
+
+    @Test
+    @DisplayName("Test diff when one el is deleted and new one is added")
+    public void whenDelete1ElAdd1El() {
+        Set<User> previous = Set.of(u1, u2);
+        Set<User> current = Set.of(u2, u3);
+        assertThat(
+                Analyze.diff(previous, current),
+                is(new Info(1, 0, 1))
+        );
+    }
+
+    @Test
+    @DisplayName("Test diff when one el is deleted and one is changed")
+    public void whenDelete1ElChange1El() {
+        Set<User> previous = Set.of(u1, u2);
+        Set<User> current = Set.of(new User(2, "BB"));
+        assertThat(
+                Analyze.diff(previous, current),
+                is(new Info(0, 1, 1))
         );
     }
 }

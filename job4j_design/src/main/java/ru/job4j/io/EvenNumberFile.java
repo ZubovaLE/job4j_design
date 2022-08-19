8ad5c8345948f9f070efa47fdd09567bbdf099ae
NikiforovaLE <n.lubovj@gmail.com>
2022-08-19 22:02:55 +0300

    Add even.txt file with different numbers in rows. Add EvenNumberFile class that read the numbers from the file and print only even numbers

---
+++ job4j_design/src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,24 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("even.txt")) {
+            StringBuilder numbers = new StringBuilder();
+            StringBuilder evenNumbers = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                numbers.append((char) read);
+            }
+            String[] lines = numbers.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                if (!line.isBlank() && Integer.parseInt(line) % 2 == 0) {
+                    System.out.println(line);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

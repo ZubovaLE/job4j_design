bcc745b3c181b518dce7a97f033a4ef6a0e71122
NikiforovaLE <n.lubovj@gmail.com>
2022-08-31 23:53:39 +0300

    Change Dir class

--- job4j_design/src/main/java/ru/job4j/io/Dir.java
+++ job4j_design/src/main/java/ru/job4j/io/Dir.java
@@ -1,14 +1,8 @@
 package ru.job4j.io;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.FileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
 
-import static java.nio.file.FileVisitResult.CONTINUE;
-
-public class Dir implements FileVisitor<File> {
+public class Dir {
     public static void main(String[] args) {
         File file = new File("c:\\projects");
         if (!file.exists()) {
@@ -20,28 +14,8 @@ public class Dir implements FileVisitor<File> {
         int totalSize = 0;
         for (File subfile : file.listFiles()) {
             totalSize += (int) subfile.length();
-            System.out.println(subfile.getAbsoluteFile() + ", size: " + subfile.length());
+            System.out.printf("name: %s, size: %d bytes%n", subfile.getAbsoluteFile(), subfile.length());
         }
-        System.out.printf("Total size: %d", totalSize);
-    }
-
-    @Override
-    public FileVisitResult preVisitDirectory(File dir, BasicFileAttributes attrs) throws IOException {
-        return CONTINUE;
-    }
-
-    @Override
-    public FileVisitResult visitFile(File file, BasicFileAttributes attrs) throws IOException {
-        return CONTINUE;
-    }
-
-    @Override
-    public FileVisitResult visitFileFailed(File file, IOException exc) throws IOException {
-        return CONTINUE;
-    }
-
-    @Override
-    public FileVisitResult postVisitDirectory(File dir, IOException exc) throws IOException {
-        return CONTINUE;
+        System.out.printf("Total size: %d bytes", totalSize);
     }
 }

2f3ec84a9fdfa91c08780b8f4f1555802ec17787
NikiforovaLE <n.lubovj@gmail.com>
2022-09-03 23:53:24 +0300

    Correct DuplicateVisitor class that finds duplicates in a folder. Change DuplicatesFinder class

--- job4j_design/src/main/java/ru/job4j/io/DuplicatesFinder.java
+++ job4j_design/src/main/java/ru/job4j/io/DuplicatesFinder.java
@@ -6,6 +6,8 @@ import java.nio.file.Path;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        Files.walkFileTree(Path.of("./"), new DuplicatesVisitor());
+        DuplicatesVisitor findDuplicates = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of("c:\\test"), findDuplicates);
+        findDuplicates.getDuplicates();
     }
 }
--- job4j_design/src/main/java/ru/job4j/io/DuplicatesVisitor.java
+++ job4j_design/src/main/java/ru/job4j/io/DuplicatesVisitor.java
@@ -5,11 +5,31 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    Map<FileProperty, List<Path>> pathsOfDuplicates = new HashMap<>();
+
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        System.out.println(file.toAbsolutePath());
+        FileProperty newFile = new FileProperty(attrs.size(), file.toFile().getName());
+        if (pathsOfDuplicates.containsKey(newFile)) {
+            pathsOfDuplicates.get(newFile).add(file);
+        } else {
+            List<Path> paths = new ArrayList<>();
+            paths.add(file);
+            pathsOfDuplicates.put(newFile, paths);
+        }
         return super.visitFile(file, attrs);
+    }
+
+    public void getDuplicates() {
+        pathsOfDuplicates.entrySet().stream()
+                .filter(p -> p.getValue().size() > 1)
+                .flatMap(e -> e.getValue().stream())
+                .forEach(file -> System.out.println(file.toAbsolutePath()));
     }
 }

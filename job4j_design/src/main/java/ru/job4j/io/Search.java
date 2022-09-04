14ea97895ff7227c5df16576f0286dea27f022e0
NikiforovaLE <n.lubovj@gmail.com>
2022-09-04 23:52:51 +0300

    Add SearchFiles class to search files by certain predicate. Improve Search class

--- job4j_design/src/main/java/ru/job4j/io/Search.java
+++ job4j_design/src/main/java/ru/job4j/io/Search.java
@@ -4,10 +4,21 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        Files.walkFileTree(start, new PrintFiles());
+        if (args.length < 2) {
+            throw new IllegalArgumentException("There are no arguments");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
     }
 }

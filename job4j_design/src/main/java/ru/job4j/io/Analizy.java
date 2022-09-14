ecbedf92b685f99ad915bfb67f9a194c5116f229
NikiforovaLE <n.lubovj@gmail.com>
2022-09-14 23:57:09 +0300

    Correct Analizy class and add one more test for it

--- job4j_design/src/main/java/ru/job4j/io/Analizy.java
+++ job4j_design/src/main/java/ru/job4j/io/Analizy.java
@@ -6,21 +6,29 @@ import java.io.FileReader;
 import java.io.PrintWriter;
 
 public class Analizy {
+    private final static String LINE_DELIMITER = " ";
+
     public void unavailable(String source, String target) {
-        boolean unavailable = false;
+        boolean stopWorking = false;
         try (PrintWriter out = new PrintWriter(new FileOutputStream(target));
              BufferedReader read = new BufferedReader(new FileReader(source))) {
             String line = read.readLine();
+            String status;
+            String time;
             while (line != null) {
                 if (!line.isBlank()) {
-                    String[] data = line.split(" ");
-                    String status = data[0];
-                    if (!unavailable && (status.equals("400") || status.equals("500"))) {
-                        out.print(data[1]);
-                        unavailable = true;
-                    } else if (unavailable && !(status.equals("400") || status.equals("500"))) {
-                        out.print(";" + data[1] + System.lineSeparator());
-                        unavailable = false;
+                    String[] data = line.split(LINE_DELIMITER);
+                    if (data.length == 2) {
+                        status = data[0];
+                        time = data[1];
+                        if (!stopWorking && downtimeStatus(status)) {
+                            out.print(time);
+                            stopWorking = true;
+                        } else if (stopWorking && !downtimeStatus(status)) {
+                            StringBuilder info = new StringBuilder();
+                            out.print(info.append(";").append(time).append(System.lineSeparator()));
+                            stopWorking = false;
+                        }
                     }
                 }
                 line = read.readLine();
@@ -28,6 +36,10 @@ public class Analizy {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private boolean downtimeStatus(String status) {
+        return status.equals("400") || status.equals("500");
     }
 
     public static void main(String[] args) {
--- job4j_design/src/test/java/ru/job4j/io/AnalizyTest.java
+++ job4j_design/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
-import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -22,6 +21,33 @@ public class AnalizyTest {
         File target = folder.newFile("target.txt");
         try (PrintWriter out = new PrintWriter(source)) {
             out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
+        }
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(l -> rsl.append(l).append("\n"));
+        }
+        assertThat(rsl.toString(), is("""
+                10:57:01;10:59:01
+                11:01:02;11:02:02
+                """));
+    }
+
+    @Test
+    @DisplayName("Test unavailable")
+    public void unavailableWhenEmptyLines() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println(" ");
+            out.println("500 ");
             out.println("500 10:57:01");
             out.println("400 10:58:01");
             out.println("200 10:59:01");

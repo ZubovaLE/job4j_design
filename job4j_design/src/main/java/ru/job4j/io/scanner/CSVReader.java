0db31f7dbeda7ba1e5f1d42391635d23425fd94d
NikiforovaLE <n.lubovj@gmail.com>
2022-09-18 22:29:09 +0300

    Improve CSVReader class

--- job4j_design/src/main/java/ru/job4j/io/scanner/CSVReader.java
+++ job4j_design/src/main/java/ru/job4j/io/scanner/CSVReader.java
@@ -2,6 +2,7 @@ package ru.job4j.io.scanner;
 
 import ru.job4j.io.ArgsName;
 
+import java.io.*;
 import java.nio.file.Paths;
 import java.util.*;
 
@@ -13,19 +14,27 @@ public class CSVReader {
     private final static String OUT = "out";
     private final static String FILTER = "filter";
     private final static String FILTER_DELIMITER = ",";
+    private final static String STDOUT = "stdout";
 
     public static void handle(ArgsName argsName) {
         String path = argsName.get(PATH);
         String delimiter = argsName.get(DELIMITER);
         String out = argsName.get(OUT);
         String filter = argsName.get(FILTER);
-        String[] tableElements;
+        String line;
+        List<String> listOfFilters;
+        List<Integer> appropriateIndexes;
         if (isValid(path, delimiter, out, filter)) {
             Optional<List<String>> filters = getListOfFilters(filter);
-            try (var scanner = new Scanner(path)) {
-                while (scanner.hasNext()) {
-                    String line = scanner.nextLine();
-                    tableElements = line.split(delimiter);
+            try (var scanner = new Scanner(path).useDelimiter(System.lineSeparator())) {
+                if (scanner.hasNext() && filters.isPresent()) {
+                    line = scanner.nextLine();
+                    listOfFilters = filters.get();
+                    appropriateIndexes = getAppropriateIndexes(line, delimiter, listOfFilters);
+                    while (scanner.hasNext()) {
+                        line = scanner.nextLine();
+                        createString(out, line, delimiter, appropriateIndexes);
+                    }
                 }
             }
         }
@@ -39,8 +48,26 @@ public class CSVReader {
         return Optional.of(Arrays.stream(stringOfFilters.split(FILTER_DELIMITER)).toList());
     }
 
-    private String createString(List<String> filters, String line, String delimiter, String out) {
-        String result = "";
-        return result;
+    private static List<Integer> getAppropriateIndexes(String line, String delimiter, List<String> filters) {
+        List<String> tableElements = Arrays.asList(line.split(delimiter));
+        List<Integer> indexesOfAppropriateElements = new ArrayList<>();
+        for (String filter : filters) {
+            indexesOfAppropriateElements.add(tableElements.indexOf(filter));
+        }
+        return indexesOfAppropriateElements;
+    }
+
+    private static String createString(String out, String line, String delimiter, List<Integer> indexes) {
+        List<String> tableElements = Arrays.asList(line.split(delimiter));
+        StringBuilder outputString = new StringBuilder();
+        for (Integer index : indexes) {
+            outputString.append(tableElements.get(index)).append(delimiter);
+        }
+        if (out.equals(STDOUT)) {
+            System.out.println(outputString);
+        } else {
+            File outputFile = new File(out);
+        }
+        return outputString.toString();
     }
 }

a400c2a6effe4c0eb73ce6097b880ca19ba37553
NikiforovaLE <n.lubovj@gmail.com>
2022-09-23 23:49:50 +0300

    Connect slf4j library to the project. Change UsageLog4j class

--- job4j_design/src/main/java/ru/job4j/io/UsageLog4j.java
+++ job4j_design/src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,10 +1,10 @@
 package ru.job4j.io;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UsageLog4j {
-    public static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
+    public static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
         LOG.trace("trace message");
@@ -12,5 +12,20 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+
+        String name = "Lyubov";
+        byte monthOfBirth = 8;
+        short dayOfBirth = 29;
+        int age = 23;
+        long id = 9876543210L;
+        float height = 168.8F;
+        double weight = 55.45;
+        char category = 'A';
+        boolean married = false;
+        LOG.debug("User info name : {}, age : {}", name, age);
+        LOG.debug("User's day of birth : {}, month : {}", dayOfBirth, monthOfBirth);
+        LOG.debug("User's id : {}", id);
+        LOG.debug("User's height : {} and weight {}", height, weight);
+        LOG.debug("Is User married?: {}, User's category: {}", married, category);
     }
 }

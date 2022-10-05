2512371495c78a9c69ff736cfc9e25e2fdab1b2c
NikiforovaLE <n.lubovj@gmail.com>
2022-10-05 23:30:03 +0300

    Reactor EchoServer class

--- job4j_design/src/main/java/ru/job4j/io/EchoServer.java
+++ job4j_design/src/main/java/ru/job4j/io/EchoServer.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import static java.util.Map.entry;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 import java.io.BufferedReader;
@@ -16,9 +17,9 @@ public class EchoServer {
     private static final String EXIT = "Bye";
     private static final String ANSWER_TO_EXIT = "Server was stopped";
 
-    private static final Map<String, String> serverAnswers = Map.of(
-            GREETING, ANSWER_TO_GREETING,
-            EXIT, ANSWER_TO_EXIT
+    private static final Map<String, String> serverAnswers = Map.ofEntries(
+            entry(GREETING, ANSWER_TO_GREETING),
+            entry(EXIT, ANSWER_TO_EXIT)
     );
 
     private static String getCurrentRequest(String line) {
@@ -27,13 +28,8 @@ public class EchoServer {
     }
 
     private static String getAnswer(String currentRequest) {
-        String answer = currentRequest;
-        for (String request : serverAnswers.keySet()) {
-            if (currentRequest.equals(request)) {
-                answer = serverAnswers.get(request);
-            }
-        }
-        return answer;
+        String answer = serverAnswers.get(currentRequest);
+        return answer != null ? answer : currentRequest;
     }
 
     public static void main(String[] args) throws IOException {
@@ -44,18 +40,18 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream())
                      )) {
-                    String answer = "I'm a simple server.";
+                    String answer;
                     String line = in.readLine();
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     if (isNotBlank(line) && line.contains("/?msg=")) {
                         String currentRequest = getCurrentRequest(line);
                         if (currentRequest.equals(EXIT)) {
                             server.close();
                         }
                         answer = getAnswer(currentRequest);
+                        out.write(answer.getBytes());
+                        out.flush();
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    out.write(answer.getBytes());
-                    out.flush();
                 }
             }
         }

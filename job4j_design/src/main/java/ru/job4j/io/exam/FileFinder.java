e58431dbca4bbb22d1b5581553f74071cf8a5e9e
NikiforovaLE <n.lubovj@gmail.com>
2022-10-10 23:21:36 +0300

    Refactor FileSearch class Add file search by regular expression in FileFinder class

--- job4j_design/src/main/java/ru/job4j/io/exam/FileFinder.java
+++ job4j_design/src/main/java/ru/job4j/io/exam/FileFinder.java
@@ -5,27 +5,38 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 public class FileFinder extends SimpleFileVisitor<Path> {
     private final String desiredFIle;
-    private final String SEARCH_TYPE;
+    private final String searchType;
     private String filePath = "File was not found";
-    private final String SEARCH_TYPE_IS_NAME = "name";
-    private final String SEARCH_TYPE_IS_MASK = "mask";
+    private final static String SEARCH_TYPE_IS_NAME = "name";
+    private final static String SEARCH_TYPE_IS_MASK = "mask";
+    private final static String SEARCH_TYPE_IS_REGEX = "regex";
 
-    public FileFinder(String desiredFIle, String SEARCH_TYPE) {
+    public FileFinder(String desiredFIle, String searchType) {
         this.desiredFIle = desiredFIle;
-        this.SEARCH_TYPE = SEARCH_TYPE;
+        this.searchType = searchType;
     }
 
     public String getFilePath() {
         return filePath;
     }
 
+    private boolean getFilePathByRegex(String fileName) throws PatternSyntaxException {
+        Pattern regexToSearchFile = Pattern.compile(desiredFIle);
+        Matcher matcher = regexToSearchFile.matcher(fileName);
+        return matcher.matches();
+    }
+
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        if ((SEARCH_TYPE.equals(SEARCH_TYPE_IS_NAME) && file.toFile().getName().equals(desiredFIle))
-                || (SEARCH_TYPE.equals(SEARCH_TYPE_IS_MASK) && (file.toFile().getName().matches(desiredFIle)))) {
+        if ((searchType.equals(SEARCH_TYPE_IS_NAME) && file.toFile().getName().equals(desiredFIle))
+                || (searchType.equals(SEARCH_TYPE_IS_MASK) && (file.toFile().getName().matches(desiredFIle)))
+        || (searchType.equals(SEARCH_TYPE_IS_REGEX) && getFilePathByRegex(file.toFile().getName()))) {
             filePath = file.toFile().getAbsolutePath();
             return FileVisitResult.SKIP_SIBLINGS;
         }
--- job4j_design/src/main/java/ru/job4j/io/exam/FileSearch.java
+++ job4j_design/src/main/java/ru/job4j/io/exam/FileSearch.java
@@ -16,15 +16,15 @@ public class FileSearch {
     private static final String FILE_NAME = "n";
     private static final String SEARCH_TYPE = "t";
     private static final String TARGET = "o";
-    private static final List<String> SEARCH_TYPES = List.of("name", "mask");
+    private static final List<String> SEARCH_TYPES = List.of("name", "mask", "regex");
 
     public static void searchFile(ArgsName argsname) throws IOException {
         String directory = argsname.get(DIRECTORY);
-        String file = argsname.get(FILE_NAME);
+        String fileName = argsname.get(FILE_NAME);
         String searchType = argsname.get(SEARCH_TYPE);
         String target = argsname.get(TARGET);
-        if (isValid(directory, file, searchType, target)) {
-            FileFinder finder = new FileFinder(file, searchType);
+        if (isArgumentsValid(directory, searchType, fileName, target)) {
+            FileFinder finder = new FileFinder(fileName, searchType);
             Files.walkFileTree(Paths.get(directory), finder);
             try (FileWriter out = new FileWriter(target)) {
                 out.write(finder.getFilePath());
@@ -35,11 +35,20 @@ public class FileSearch {
         }
     }
 
-    private static boolean isValid(String directory, String file, String searchType, String target) {
-        return isNotBlank(directory) && new File(directory).exists()
-                && new File(directory).isDirectory() && isNotBlank(file)
-                && isNotBlank(searchType) && (SEARCH_TYPES.contains(searchType))
+    private static boolean isArgumentsValid(String directory, String searchType, String fileName, String target) {
+        return isDirectoryValid(directory)
+                && isSearchTypeValid(searchType)
+                && isNotBlank(fileName)
                 && isNotBlank(target);
+    }
+
+    private static boolean isDirectoryValid(String directory) {
+        return isNotBlank(directory) && new File(directory).exists()
+                && new File(directory).isDirectory();
+    }
+
+    private static boolean isSearchTypeValid(String searchType) {
+        return isNotBlank(searchType) && SEARCH_TYPES.contains(searchType);
     }
 
     public static void main(String[] args) throws IOException {

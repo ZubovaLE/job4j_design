3c06c6b4dc6f86a2fe3713d34fbfd257594d423c
NikiforovaLE <n.lubovj@gmail.com>
2022-10-12 23:56:57 +0300

    Add tests for FileSearch class

--- job4j_design/src/test/java/ru/job4j/io/exam/FileSearchTest.java
+++ job4j_design/src/test/java/ru/job4j/io/exam/FileSearchTest.java
@@ -6,18 +6,47 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import ru.job4j.io.ArgsName;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 import static org.assertj.core.api.Assertions.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 
 class FileSearchTest {
-
     @ParameterizedTest
-    @CsvSource(value = {"1, 2, 3, 4"})
-    @DisplayName("When invalid arguments then get exception")
+    @CsvSource(value = {"-dir = di, -n=fileName, -t=name, -o=out.txt",
+            "-d=c:\\projects, -name=fileName;, -t=name, -o=out.txt",
+            "-d=c:\\projects, -n=fileName, -type=searchType, -o=out.txt",
+            "-d=c:\\projects, -n=fileName, -t=name, -out=out.txt",
+            "-d=c:\\projects\\job4j_design\\pom.xml, -n=fileName, -t=type, -o=out.txt",
+            "-d=p:\\, -n=fileName, -t=type, -o=out.txt",
+            "-d=c:\\projects, -n=fileName, -t=type, -o=out.txt"
+    }, nullValues = "null")
+    @DisplayName("When invalid arguments then get IllegalArgumentException")
     void searchFile(String directory, String fileName, String searchType, String target) {
         String[] args = {directory, fileName, searchType, target};
-        assertThatIllegalArgumentException().isThrownBy(() -> ArgsName.of(args)).withMessage("Incorrect arguments");
+        ArgsName argsname = ArgsName.of(args);
+        assertThatIllegalArgumentException().isThrownBy(() -> FileSearch.searchFile(argsname)).withMessage("Incorrect arguments");
+    }
+
+    @Test
+    @DisplayName(" ")
+    void whenValid() throws IOException {
+        Path tempDirectory = Files.createTempDirectory("tempDirectory");
+        Path tmpFile1 = Files.createTempFile(tempDirectory, "firstTestFile.txt", null);
+        Path tmpFile2 = Files.createTempFile(tempDirectory, "secondTestFile", ".txt");
+        Path tmpOutFile = Files.createTempFile(tempDirectory, "outFile.txt", null);
+        String[] args = {"-d=tempDirectory, -n=firstTestFile, -t=name, -o=outFile.txt"};
+        ArgsName argsname = ArgsName.of(args);
+//        FileSearch.searchFile(argsname);
+        String expected = "tempDirectory\\firstTestFile.txt";
+        Scanner scanner = new Scanner(tmpOutFile).useDelimiter(System.lineSeparator());
+        String result ="";
+        while (scanner.hasNext()) {
+            result = scanner.next();
+        }
+        scanner.close();
+        assertThat(result).isEqualTo(expected);
     }
 }

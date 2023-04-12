869629182200f2a301739e24bc27709e0e045033
NikiforovaLE <n.lubovj@gmail.com>
2023-04-12 21:27:42 +0300

    Improve project Add lombock. Improve Person class

--- src/main/java/ru/job4j/io/serialization/Person.java
+++ src/main/java/ru/job4j/io/serialization/Person.java
@@ -1,15 +1,17 @@
 package ru.job4j.io.serialization;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.xml.bind.annotation.*;
-import java.io.StringWriter;
 import java.util.Arrays;
 import java.util.Objects;
 
 @XmlRootElement(name = "person")
 @XmlAccessorType(XmlAccessType.FIELD)
+@NoArgsConstructor
+@Getter
 public class Person {
     @XmlAttribute
     private boolean sex;
@@ -19,30 +21,11 @@ public class Person {
 
     private String[] statuses;
 
-    public Person() {
-    }
-
     public Person(boolean sex, int age, Contact contact, String... statuses) {
         this.sex = sex;
         this.age = age;
         this.contact = contact;
         this.statuses = statuses;
-    }
-
-    public boolean isSex() {
-        return sex;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public Contact getContact() {
-        return contact;
-    }
-
-    public String[] getStatuses() {
-        return statuses;
     }
 
     @Override
